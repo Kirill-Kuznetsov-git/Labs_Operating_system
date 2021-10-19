5a5ce716857c934768b12586837bf56998437662
Kirill-Kuznetsov-git <57671707+Kirill-Kuznetsov-git@users.noreply.github.com>
2021-10-19 23:32:10 +0300

    Add files via upload

--- week9/ex1.c
+++ week9/ex1.c
@@ -20,24 +20,22 @@ int main() {
     bool flag = false;
 
     int applicant_i = 0;
+    int applicant_id;
     uint16_t applicant_r_clock = UINT16_MAX;
-
+    printf("Print the number of pages\n");
     while(fscanf(file, "%d", &current_id_page) != EOF){
         number_ref++;
         flag = false;
         applicant_i = 0;
+        applicant_id = -1;
         applicant_r_clock = UINT16_MAX;
 
         for (int i = 0; i < number_pages;i++){
             printf("i = %d id = %d %d\n", i, id_pages[i], r_clock[i]);
         }
-        printf("Current page number %d\n\n", current_id_page);
+        printf("current page number %d\n\n", current_id_page);
 
         for (int i = 0;i < number_pages;i++){
-            if (applicant_r_clock > r_clock[i]){
-                applicant_r_clock = r_clock[i];
-                applicant_i = i;
-            }
             if (id_pages[i] != current_id_page && id_pages[i] != 0){
                 r_clock[i] >>= 1;
             }
@@ -49,6 +47,13 @@ int main() {
             }
         }
         if (flag == false){
+            for (int i = 0;i < number_pages;i++){
+                if (applicant_r_clock >= r_clock[i] && ((applicant_id != 0) || (applicant_id == 0 && id_pages[i] == 0))){
+                    applicant_r_clock = r_clock[i];
+                    applicant_id = id_pages[i];
+                    applicant_i = i;
+                }
+            }
             id_pages[applicant_i] = current_id_page;
             r_clock[applicant_i] = pow(2, 15);
         }
